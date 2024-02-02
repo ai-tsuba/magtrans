6b4d55c423c5cabd0b5d0d537fcfeeba2ad63e5c
ai-tsuba <6925458@gmail.com>
2024-02-02 15:59:40 +0300

    зкщигуь

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
 function displayFrame() {
-    document.getElementsByClassName("topform").innerHTML = "New text!";
+    document.getElementsByClassName("topform")[0].innerHTML = "<iframe src='https://zlecenia.magtrans.eu/' uk-cover></iframe>";
 }
+
